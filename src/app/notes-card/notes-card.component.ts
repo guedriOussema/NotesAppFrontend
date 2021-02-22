c1814e27a37470f1637bf679bcfa10633a24b4be
guedriOussema <guedroussemamath@gmail.com>
2021-02-23 00:40:59 +0100

    testing notes details page

--- src/app/notes-card/notes-card.component.ts
+++ src/app/notes-card/notes-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -10,6 +10,9 @@ export class NotesCardComponent implements OnInit {
 
   @Input() title:string;
   @Input() body: string;
+  @Input() link: string;
+
+  @Output('delete') deleteEvent: EventEmitter<void> = new EventEmitter<void>();
 
   @ViewChild('truncator', {static: true}) truncator: ElementRef<HTMLElement>;
   @ViewChild('bodyText', {static: true}) bodyText: ElementRef<HTMLElement>;
@@ -25,6 +28,11 @@ export class NotesCardComponent implements OnInit {
     } else {
       this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
     }
+  }
+
+
+  onXButtonClick() {
+    this.deleteEvent.emit();
   }
 
 }
